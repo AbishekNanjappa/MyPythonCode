27d0300e83cef19f7589686b67f06e9eea3ce476
AbishekNanjappa <67585189+AbishekNanjappa@users.noreply.github.com>
2020-10-17 23:43:50 +0530

    Add files via upload

---
+++ triangle/main.py
@@ -0,0 +1,7 @@
+import utility
+print("Enter the lengths of the three sides of the triangle:")
+a = int(input(""))
+b = int(input(""))
+c = int(input(""))
+return_val = utility.triangle_type(a,b,c)
+print(return_val)
